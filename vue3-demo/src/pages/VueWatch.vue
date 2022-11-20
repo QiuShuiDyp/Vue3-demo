0c42da67974378dd637d1fc370cefd25af2cbe48
QiuShuiDyp <993865890@qq.com>
2022-11-20 17:56:07 +0800

    feat: watch demo

--- vue3-demo/src/pages/VueWatch.vue
+++ vue3-demo/src/pages/VueWatch.vue
@@ -1,3 +1,13 @@
+<!--
+ * @Author: QiuShui
+ * @Date: 2022-11-08 07:15:15
+ * @LastEditors: Qiushui
+ * @LastEditTime: 2022-11-08 07:38:23
+ * @FilePath: /vue3-demo/src/pages/VueWatch.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by 用户/公司名, All Rights Reserved. 
+-->
 <template>
   <input type="text" v-model="num1" />
   +
@@ -12,6 +22,7 @@ const num2 = ref(0);
 const state = reactive({
   count: 0,
 });
+let arr = reactive([0])
 // 计算属性
 const result = computed(() => {
   return parseInt(num1.value || 0) + parseInt(num2.value || 0);
@@ -20,9 +31,12 @@ const result = computed(() => {
 watch(result, () => {
   if (result.value > 100) console.log(`${result.value}超过了100`);
 });
+
 // 监听多个属性
 watch([num1, num2], ([newNum1, newNum2]) => {
   state.count++;
+  arr = [...arr, state.count]
+  // arr.push(state.count)
   console.log(parseInt(newNum1) * parseInt(newNum2));
 });
 // 监听getter函数,当监听响应式对象的属性值时，不能直接监听
@@ -33,6 +47,13 @@ watch(
     console.log(count, "count");
   }
 );
+// 监听响应式对象，会隐式地创建一个深层侦听器，当对象的属性发生改变的时候，触发侦听的回调。newVal，oldVal相等
+watch(state, (newVal,oldVal)=>{
+  console.log(state.count, newVal, oldVal,'state')
+})
+watch(arr,(newVal,oldVal)=>{
+  console.log(arr, newVal, oldVal,'arr')
+})
 </script>
 
 <style></style>
